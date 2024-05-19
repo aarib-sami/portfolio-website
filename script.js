bb7da655ea68e8fb7ec25e314712da87e582b064
Aarib Sami <134624373+aarib-sami@users.noreply.github.com>
2024-05-19 01:08:31 -0400

    Introducing video playback.

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ function toggleMenu()
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function openModal(videoUrl) {
+    var modal = document.getElementById("videoModal");
+    var video = document.getElementById("demoVideo");
+    var source = document.getElementById("videoSource");
+    
+    source.src = videoUrl;
+    video.load();
+    
+    modal.style.display = "block";
+  }
+
+  function closeModal() {
+    var modal = document.getElementById("videoModal");
+    var video = document.getElementById("demoVideo");
+    
+    modal.style.display = "none";
+    video.pause();
+  }
+
+  // Close modal when clicking outside of the modal content
+  window.onclick = function(event) {
+    var modal = document.getElementById("videoModal");
+    if (event.target == modal) {
+      closeModal();
+    }
+  }
