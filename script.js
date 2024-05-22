b73f5d638fd0810dfc5d649cef6307453c787565
Aarib Sami <134624373+aarib-sami@users.noreply.github.com>
2024-05-22 01:04:00 -0400

    Added pictures for all projects, fixed media queries issue.

--- script.js
+++ script.js
@@ -6,32 +6,4 @@ function toggleMenu()
     icon.classList.toggle("open");
 }
 
- function openModal(videoUrl) {
-    var modal = document.getElementById("videoModal");
-    var video = document.getElementById("demoVideo");
-    var source = document.getElementById("videoSource");
-    
-    source.src = videoUrl;
-    video.load();
-    
-    modal.style.display = "block";
-  }
-
-  function closeModal() {
-    var modal = document.getElementById("videoModal");
-    var video = document.getElementById("demoVideo");
-  
-    modal.style.display = "none";
-    video.pause();
-  
-    // Prevent default behavior of the click event
-    event.preventDefault();
-  }
-
-  // Close modal when clicking outside of the modal content
-  window.onclick = function(event) {
-    var modal = document.getElementById("videoModal");
-    if (event.target == modal) {
-      closeModal();
-    }
-  }
+ 
