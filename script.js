be75126682636e9ac93abfc53c14c2fe9dff41a1
Aarib Sami <134624373+aarib-sami@users.noreply.github.com>
2024-05-23 01:09:30 -0400

    Finished adding all details of projects.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function toggleMenu()
+{
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
   // Video modal functionality
   var videoModal = document.getElementById("videoModal");
